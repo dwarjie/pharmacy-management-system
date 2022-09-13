77a9105162762b9fdfaf6e934e4a08a4ef4cd875
unknown <macoycoder@gmail.com>
2022-09-14 00:26:30 +0800

    Started Side Navigation

--- client/src/components/main/MainComponents.js
+++ client/src/components/main/MainComponents.js
@@ -2,11 +2,13 @@ import React from "react";
 
 // components
 import TopNavigation from "./navigation/TopNavigation";
+import SideNavigation from "./navigation/SideNavigation";
 
 const MainComponent = () => {
 	return (
 		<div>
 			<TopNavigation />
+			<SideNavigation />
 		</div>
 	);
 };
---
+++ client/src/components/main/navigation/SideNavigation.js
@@ -0,0 +1,14 @@
+// This component containers the SideNavigation Bar
+// Hidden by default in mobile and Tablet
+
+const SideNavigation = () => {
+	return (
+		<nav className="position-absolute top-0 start-0 navbar bg-light w-50 height-auto">
+			<div className="container-fluid">
+				<h2>ActiveCare Home Health Solution Inc.</h2>
+			</div>
+		</nav>
+	);
+};
+
+export default SideNavigation;
