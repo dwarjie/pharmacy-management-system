4c4fb84951913a4f625c3867b67d8899f361ef4e
unknown <macoycoder@gmail.com>
2022-12-05 12:39:31 +0800

    [IMP] PO UI

--- client/src/components/main/navigation/purchase_order/PurchaseOrder.js
+++ client/src/components/main/navigation/purchase_order/PurchaseOrder.js
@@ -1,18 +1,62 @@
 // This component will add a purchase order
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import DropDownDefaultOption from "../../../layout/DropDownDefaultOption.layout";
 import MedicineService from "../../../../services/MedicineService";
 
+// icons
+import { MdDelete } from "react-icons/md";
+import SupplierService from "../../../../services/SupplierService";
+import { getCurrentDate } from "../../../../helper/dateHelper";
+import parseDropdownValue from "../../../../helper/parseJSON";
+
 const PurchaseOrder = () => {
+	const initialPurchaseOrder = {
+		id: null,
+		POCode: "",
+		OrderDate: getCurrentDate(),
+		ItemQty: 0,
+		Status: "pending",
+		Total: 0,
+		supplierId: null,
+	};
+
+	const initialDropDownValue = {
+		supplier: "",
+	};
+
+	const [puchaseOrder, setPurchaseOrder] = useState(initialPurchaseOrder);
 	const [searchProduct, setSearchProduct] = useState("");
 	const [supplierProducts, setSupplierProducts] = useState([]);
 	const [orderList, setOrderList] = useState([]);
+	const [supplierList, setSupplierList] = useState([]);
+	const [activeDropDownValue, setActiveDropDownValue] =
+		useState(initialDropDownValue);
+
+	useEffect(() => {
+		getALlSuppliers();
+	}, []);
 
 	// get all the products in search
 	const getAllProducts = () => {
-		MedicineService.getByTitleAndSupplier(searchProduct, 1)
+		MedicineService.getByTitleAndSupplier(
+			searchProduct,
+			puchaseOrder.supplierId
+		)
 			.then((response) => {
 				console.log(response.data);
 				setSupplierProducts(response.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	// get all the suppliers
+	const getALlSuppliers = () => {
+		SupplierService.getSupplier()
+			.then((response) => {
+				console.log(response.data);
+				setSupplierList(response.data);
 			})
 			.catch((err) => {
 				console.log(err);
@@ -37,15 +81,19 @@ const PurchaseOrder = () => {
 		if (!checkOrderExist(selectedProduct)) {
 			// not yet exist
 			let initialSelectedProduct = {
+				PCode: selectedProduct.ProductCode,
+				Item: selectedProduct.ProductName,
+				OnHand: selectedProduct.Quantity,
+				ReorderPoint: selectedProduct.ReorderPoint,
 				Quantity: 1,
+				UnitCost: selectedProduct.SellingPrice,
 				Total: selectedProduct.SellingPrice,
+				ReceivedData: "",
 				medicineId: selectedProduct.id,
 				purchaseId: 0,
-				product: selectedProduct.medicine,
 			};
 			setOrderList([...orderList, initialSelectedProduct]);
 		}
-		console.log(selectedProduct);
 	};
 
 	// handle the searching
@@ -60,16 +108,27 @@ const PurchaseOrder = () => {
 			<div className="d-flex flex-column flex-md-row justify-content-start gap-3">
 				<OrderInformation
 					searchProduct={searchProduct}
-					handleSearchProduct={handleSearchProduct}
+					supplierList={supplierList}
+					purchaseOrder={puchaseOrder}
+					activeDropDownValue={activeDropDownValue}
 					supplierProducts={supplierProducts}
+					handleSearchProduct={handleSearchProduct}
 					addProduct={addProduct}
+					setPurchaseOrder={setPurchaseOrder}
+					setActiveDropDownValue={setActiveDropDownValue}
 				/>
 			</div>
 			<div className="h-75 border border-dark rounded simple-shadow mt-3">
 				<div className="table-responsive max-height-100">
-					<ProductTable addProduct={addProduct} />
+					<ProductTable orderList={orderList} setOrderList={setOrderList} />
 				</div>
 			</div>
+			<button
+				type="submit"
+				className="btn btn-primary col-12 col-md-1 simple-shadow mt-3 me-3"
+			>
+				Create
+			</button>
 		</div>
 	);
 };
@@ -77,8 +136,13 @@ const PurchaseOrder = () => {
 const OrderInformation = ({
 	searchProduct,
 	supplierProducts,
+	activeDropDownValue,
+	purchaseOrder,
+	supplierList,
 	handleSearchProduct,
 	addProduct,
+	setPurchaseOrder,
+	setActiveDropDownValue,
 }) => {
 	const searchData = () => {
 		if (searchProduct === "") return;
@@ -107,6 +171,7 @@ const OrderInformation = ({
 						className="form-control form-input"
 						placeholder="Search Product"
 						name="searchProduct"
+						disabled={purchaseOrder.supplierId ? false : true}
 						value={searchProduct}
 						onChange={handleSearchProduct}
 						required
@@ -126,7 +191,14 @@ const OrderInformation = ({
 				<input
 					type="text"
 					className="form-control form-input"
-					placeholder="Search Product"
+					placeholder="Purchase Code"
+					value={purchaseOrder.POCode}
+					onChange={(event) =>
+						setPurchaseOrder((prevState) => ({
+							...prevState,
+							POCode: event.target.value,
+						}))
+					}
 					required
 				/>
 			</div>
@@ -134,15 +206,71 @@ const OrderInformation = ({
 				<label className="required" htmlFor="">
 					Supplier:
 				</label>
-				<select name="" id="" className="form-select form-input">
-					<option value="">Supplier</option>
+				<select
+					name="supplierId"
+					className="form-select form-input"
+					value={activeDropDownValue.supplier}
+					onChange={(event) => {
+						let data = parseDropdownValue(event);
+						setActiveDropDownValue((prevState) => ({
+							...prevState,
+							supplier: data.SupplierName,
+						}));
+						setPurchaseOrder((prevState) => ({
+							...prevState,
+							supplierId: data.id,
+						}));
+					}}
+				>
+					<DropDownDefaultOption content={"Select Supplier"} />
+					{supplierList &&
+						supplierList.map((supplier, index) => (
+							<option
+								className="dropdown-item"
+								value={supplier.SupplierName}
+								key={index}
+								data-value={JSON.stringify(supplier)}
+							>
+								{supplier.SupplierName}
+							</option>
+						))}
 				</select>
 			</div>
 		</>
 	);
 };
 
-const ProductTable = () => {
+const ProductTable = ({ orderList, setOrderList }) => {
+	const orderData = () => {
+		return (
+			orderList &&
+			orderList.map((order, index) => (
+				<tr key={index}>
+					<td>{order.PCode}</td>
+					<td>{order.Item}</td>
+					<td>{order.OnHand}</td>
+					<td>{order.ReorderPoint}</td>
+					<td>
+						<input
+							type="number"
+							inputMode="numeric"
+							min={1}
+							className="form-control w-20 p-1"
+							// value={order.Quantity}
+						/>
+					</td>
+					<td>{order.UnitCost}</td>
+					<td>100</td>
+					<td>
+						<span className="px-1">
+							<MdDelete className="icon-size-sm cursor-pointer" />
+						</span>
+					</td>
+				</tr>
+			))
+		);
+	};
+
 	return (
 		<table className="table">
 			<thead>
@@ -153,17 +281,20 @@ const ProductTable = () => {
 					<th scope="col">Reorder</th>
 					<th scope="col">Qty</th>
 					<th scope="col">Unit Cost</th>
+					<th scope="col">Total</th>
 					<th scope="col">Action</th>
 				</tr>
 			</thead>
 			<tbody>
+				{orderData()}
 				<tr>
 					<td className="no-line"></td>
 					<td className="no-line"></td>
 					<td className="no-line"></td>
 					<td className="no-line"></td>
+					<td className="no-line"></td>
 					<td className="no-line text-center">
-						<strong>Total</strong>
+						<strong>Total:</strong>
 					</td>
 					<td className="no-line text-right">&#8369; 100.00</td>
 				</tr>
